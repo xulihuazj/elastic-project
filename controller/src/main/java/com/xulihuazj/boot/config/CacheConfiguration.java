38bfdc43ac5991a338c7d53b40cee0c2f9355d6e
xulihua <xulihua@52mamahome.com>
2018-01-08 16:35:00 +0800

    修改 pom.xml 添加 ehcache的支持

--- controller/src/main/java/com/xulihuazj/boot/config/CacheConfiguration.java
+++ controller/src/main/java/com/xulihuazj/boot/config/CacheConfiguration.java
@@ -8,6 +8,8 @@ package com.xulihuazj.boot.config;
 
 
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
